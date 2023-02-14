aa222a5f188b6fe521952f745abe5cd234cc76c7
Mirone Saul <Saul-Mirone@outlook.com>
2023-02-14 22:44:29 +0800

    remove console log

--- src/vital.ts
+++ src/vital.ts
@@ -1,3 +1,5 @@
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+
 const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals';
 
 function getConnectionSpeed() {
@@ -9,7 +11,6 @@ function getConnectionSpeed() {
     : '';
 }
 
-console.log(__VERCEL_ANALYTICS_ID__)
 export function sendToVercelAnalytics(metric: any) {
   const analyticsId = __VERCEL_ANALYTICS_ID__;
   if (!analyticsId) {
@@ -43,12 +44,10 @@ export function sendToVercelAnalytics(metric: any) {
 
 export const reportWebVitals = (onPerfEntry: (metric: any) => void) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
-      import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
-      });
+        onCLS(onPerfEntry);
+        onFID(onPerfEntry);
+        onFCP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
     }
   };
