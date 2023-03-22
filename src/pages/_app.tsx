1af3ccf33c98a5c560d647d72bf137d76b8f4b7a
Mirone <Saul-Mirone@outlook.com>
2023-03-22 16:35:34 +0800

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import Footer from "@/components/footer";
 import Nav from "@/components/nav";
 import { LayoutProvider } from "@/providers";
-import type { AppProps } from "next/app";
-
-import "@milkdown/theme-nord/style.css";
+import "@/styles/docsearch.css";
 
 import "@/styles/globals.css";
-import "@/styles/docsearch.css";
-import "@/styles/prosemirror.css";
 import "@/styles/prose.css";
+import "@/styles/prosemirror.css";
+
+import "@milkdown/theme-nord/style.css";
+import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Nav />
       <Component {...pageProps} />
       <Footer />
+      <Analytics />
     </LayoutProvider>
   );
 }
