48c386e3470fafe62a54bfa8be6bf88a83ee4df9
Saul-Mirone <Saul-Mirone@outlook.com>
2025-05-18 14:50:22 +0800

    chore: fix

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -16,8 +16,7 @@ async function main() {
 
         // Build TypeScript files
         console.log('Building TypeScript files...');
-        execSync('pnpm -F @milkdown/dev build', { stdio: 'inherit' });
-        execSync('pnpm -F @milkdown/docs build', { stdio: 'inherit' });
+        execSync('pnpm build:tsc && pnpm --filter=@milkdown/docs build', { stdio: 'inherit' });
 
         // Ensure api directory exists
         if (!existsSync(API_DIR)) {
